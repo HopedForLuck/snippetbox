973f43fb98e911ce0b7aa53ab784cab2da47c088
achurilov <achurilov@radiofid.ru>
2025-02-11 19:26:42 +0300

    logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,10 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	port := flag.String("port", ":4000", "HTTP network address")
 	flag.Parse()
@@ -16,19 +20,13 @@ func main() {
 		AddSource: true,
 	}))
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	app := &application{
+		logger: logger,
+	}
 
 	logger.Info("starting server", slog.Any("port", *port))
 
-	err := http.ListenAndServe(*port, mux)
+	err := http.ListenAndServe(*port, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
